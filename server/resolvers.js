923eb25aed1f864561afe9337a5a76584e892412
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-02-01 00:01:39 -0500

    Adding in the JWT for signing in and signing up and returning the token

--- server/resolvers.js
+++ server/resolvers.js
@@ -8,6 +8,18 @@
  */
 
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+/**
+ * Creating a function to create a JWT token for us
+ * @param { user } Object of the user being signed in or signed up
+ * @param { secret } String that is a secret only to us using the application (Comes from ENV variable)
+ * @param { expireIn } String denoting the amount of time that the token is valid for
+ */
+const createToken = (user, secret, expiresIn) => {
+    const { username, email } = user;
+    return jwt.sign({ username, email }, secret, { expiresIn });
+};
 
 module.exports = {
     Query: {
@@ -38,34 +50,36 @@ module.exports = {
             return newPost;
         },
         signinUser: async (_, { username, password }, { User }) => {
+            // Finding the user in the database
             const user = await User.findOne({ username });
             if (!user) {
                 throw new Error('User not found');
             }
-
+            // Checking to see if the users password is valid
             const isValidPassword = await bcrypt.compare(
                 password,
                 user.password
             );
-
             if (!isValidPassword) {
                 throw new Error('Invalid password');
             }
 
-            return user;
+            return { token: createToken(user, process.env.SECRET, '1hr') };
         },
         signupUser: async (_, { username, email, password }, { User }) => {
+            // Checking to make sure the user has not already signed up in the database
             const user = await User.findOne({ username });
             if (user) {
                 throw new Error('User already exists');
             }
+            // Creating the new user object and saving the user to the database
             const newUser = await new User({
                 username,
                 email,
                 password
             }).save();
 
-            return newUser;
+            return { token: createToken(newUser, process.env.SECRET, '1hr') };
         }
     }
 };
