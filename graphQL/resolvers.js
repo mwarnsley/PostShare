4f18785578427e9dc5b82a71f340495d0e31ca5e
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-01-31 19:35:34 -0500

    Adding in the resolver and mutation for adding in a post

--- graphQL/resolvers.js
+++ graphQL/resolvers.js
@@ -12,6 +12,21 @@ module.exports = {
         getUser: () => console.log('User')
     },
     Mutation: {
+        addPost: async (
+            _,
+            { title, imageUrl, categoires, description, creatorId },
+            { Post }
+        ) => {
+            const newPost = await new Post({
+                title,
+                imageUrl,
+                categoires,
+                description,
+                createdBy: creatorId
+            }).save();
+
+            return newPost;
+        },
         signupUser: async (_, { username, email, password }, { User }) => {
             const user = await User.findOne({ username });
             if (user) {
