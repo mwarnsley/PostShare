d62b52bf352ba0076a3a0a85f19507249b24072b
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-01-31 22:40:50 -0500

    Adding in the carousel into the home component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,25 +1,12 @@
 <template>
-    <v-container>
-        <h1>Home</h1>
-
-        <ApolloQuery :query="getPostsQuery">
-
-            <template slot-scope="{ result: { loading, error, data, networkStatus } }">
-                <div v-if="loading">Loading...</div>
-                <div v-else-if="error">Error! {{ error.message }}</div>
-                <div v-else-if="networkStatus">Network Status: {{ networkStatus }}</div>
-                <ul v-else v-for="post in data.getPosts" :key="post._id">
-                    <li>
-                        {{ post.title }}
-                        {{ post.imageUrl }}
-                        {{ post.description }}
-                        {{ post.likes }}
-                    </li>
-                </ul>
-            </template>
-
-        </ApolloQuery>
-
+    <v-container text-xs-center>
+        <v-flex xs12>
+            <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+                <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
+                    <h1 class="carousel-title">{{ post.title }}</h1>
+                </v-carousel-item>
+            </v-carousel>
+        </v-flex>
     </v-container>
 </template>
 
@@ -30,7 +17,12 @@
         name: 'Home',
         data() {
             return {
-                getPostsQuery: gql`
+                posts: []
+            };
+        },
+        apollo: {
+            getPosts: {
+                query: gql`
                     query {
                         getPosts {
                             _id
@@ -40,8 +32,26 @@
                             likes
                         }
                     }
-                `
+                `,
+                result({ data, loading }) {
+                    if (!loading) {
+                        this.posts = data.getPosts;
+                    }
+                }
             }
         }
     }
 </script>
+
+<style lang="scss" scoped>
+    .carousel-title {
+        background-color: rgba(0, 0, 0, 0.5);
+        border-radius: 5px 5px 0 0;
+        bottom: 50px;
+        color: #fff;
+        left: 0;
+        margin: 0 auto;
+        position: absolute;
+        right: 0;
+    }
+</style>
