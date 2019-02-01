a588a4381a72494da720bc7f60ecdb7d3c897674
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2019-01-31 22:27:43 -0500

    Fixing a type def along with adding in the ApolloQuery component to the home component

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,14 +1,25 @@
 <template>
     <v-container>
         <h1>Home</h1>
-        <ul v-for="post in getPosts" :key="post.title">
-            <li>
-                {{ post.title }}
-                {{ post.imageUrl }}
-                {{ post.description }}
-            </li>
-            <li>{{ post.likes }}</li>
-        </ul>
+
+        <ApolloQuery :query="getPostsQuery">
+
+            <template slot-scope="{ result: { loading, error, data, networkStatus } }">
+                <div v-if="loading">Loading...</div>
+                <div v-else-if="error">Error! {{ error.message }}</div>
+                <div v-else-if="networkStatus">Network Status: {{ networkStatus }}</div>
+                <ul v-else v-for="post in data.getPosts" :key="post._id">
+                    <li>
+                        {{ post.title }}
+                        {{ post.imageUrl }}
+                        {{ post.description }}
+                        {{ post.likes }}
+                    </li>
+                </ul>
+            </template>
+
+        </ApolloQuery>
+
     </v-container>
 </template>
 
@@ -17,11 +28,12 @@
 
     export default {
         name: 'Home',
-        apollo: {
-            getPosts: {
-                query: gql`
+        data() {
+            return {
+                getPostsQuery: gql`
                     query {
                         getPosts {
+                            _id
                             title
                             imageUrl
                             description
